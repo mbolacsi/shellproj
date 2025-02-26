1968a863f435a49ad54b98f2c6ec6ca5826d9e60
Michael Olacsi <michaelolacsi@u.boisestate.edu>
2025-02-25 21:35:27 -0700

    Got more commands working 💻 💣 thx to suggestion by Aarik G 🐛🔫

--- src/lab.c
+++ src/lab.c
@@ -147,7 +147,7 @@ bool do_builtin(struct shell *sh, char **argv) {
         return true;
     } else {
         // Look at all the 3 things you can do!!!!
-        if(strcmp(argv[0], "ls") != 0){ // NOW ITS 4!!! Thanks for the tip Aarik, u the GOAT! ls be working✨🔥✨
+        if(strcmp(argv[0], "ls") != 0  && strcmp(argv[0], "ps") != 0 && strcmp(argv[0], "pwd") != 0 ){ // NOW ITS 6!!! Thanks for the tip Aarik, u the GOAT! ls and ps be working✨🔥✨
         printf("Unknown command: %s\n", argv[0]);
         printf("Available commands:\n");
         printf("  exit    - Exit the shell\n");
